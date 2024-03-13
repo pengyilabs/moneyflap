2b85412e556fd28ed736259e08077683a4fc97d3
Robyifan孟一凡 <robyifanmengyifan@RobyifanmengyifandeMacBook-Air.local>
2024-03-13 09:05:34 -0600

    initial setup

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///Users/fabs/Desktop/code/moneyflap/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export { default } from "file:///Users/robyifanmengyifan/Documents/Codes/Moneyflap/moneyflap/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
