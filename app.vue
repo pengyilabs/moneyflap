06220d3ee8cbe84b7ab4ffa7a3d76379d662fdec
Fabian Mora Robles <fabianmorar1223@gmail.com>
2024-09-18 10:21:15 -0600

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://cdn.weglot.com/weglot.min.js'
+  script.async = true
+  script.onload = () => {
+    Weglot.initialize({
+      api_key: 'wg_3ee95520eaf5bc7bb4ebe7595494a9008',
+    })
+  }
+  document.head.appendChild(script)
+})
+</script>
+
 <template>
   <div
     style="
