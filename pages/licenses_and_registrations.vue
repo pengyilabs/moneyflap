837d4cae9008c3a1a3019b9c65b0671e2269fda7
dowar <gonzalo@dowar.xyz>
2024-12-12 11:53:52 -0600

    Improve formatting and readability in licenses_and_registrations.vue

--- pages/licenses_and_registrations.vue
+++ pages/licenses_and_registrations.vue
@@ -5,7 +5,7 @@
     >
       <div class="container my-5 col">
         <h2 class="fw-bolder display-2 mt-1">Licenses and Registrations</h2>
-        
+
         <h3>
           <img src="@/src/assets/us-flag.png" height="33px" />
           United States
@@ -27,10 +27,20 @@
         </h3>
         <p class="pb-3">
           <strong>MoneyFlap Digital Labs, Inc.</strong> is registered as a
-          <a target="_blank" href="https://www.cnv.gov.ar/SitioWeb/ProveedoresServiciosActivosVirtuales/RegistrosPSAV">
+          <a
+            target="_blank"
+            href="https://www.cnv.gov.ar/SitioWeb/ProveedoresServiciosActivosVirtuales/RegistrosPSAV"
+          >
             Virtual Assets Service Provider (VASP)
           </a>
-          under No. D1-2024-135-APN-GRC#CNV dated 21st August 2024 in the Registry of Virtual Asset Service Providers of National Securities Commission (CNV). This registration is for the purposes of control as an Obligated Subject before the Financial Information Unit (UIF) and any other regulatory entity authorized for this purpose, within the framework of its powers, and does not imply a license or supervision by the National Securities Commission (CNV) on the activity carried out by MoneyFlap. 
+          under No. D1-2024-135-APN-GRC#CNV dated 21st August 2024 in the
+          Registry of Virtual Asset Service Providers of National Securities
+          Commission (CNV). This registration is for the purposes of control as
+          an Obligated Subject before the Financial Information Unit (UIF) and
+          any other regulatory entity authorized for this purpose, within the
+          framework of its powers, and does not imply a license or supervision
+          by the National Securities Commission (CNV) on the activity carried
+          out by MoneyFlap.
         </p>
 
         <h3>
@@ -38,7 +48,15 @@
           Senegal and West African Economic and Monetary Union (WAEMU)
         </h3>
         <p class="pb-3">
-          <strong>MoneyFlap Senegal SAS</strong>  operates as a technology services provider. It is neither licensed by the Central Bank of the West African States (BCEAO) nor by the “Autorité des Marchés Financiers de l’UMOA (AMF-UMOA)”, also known as the “Conseil Regional de l’Epargne Publique et des Marches Financiers (CREPMF)”. However, it is self-considered as an “Entreprise et Profession Non Financière Désignée (EPNFD)” and is a reporting entity to the Financial Intelligence Unit (CENTIF), under the control of the Ministry of Economy and Finances, for anti-money laundering purposes.
+          <strong>MoneyFlap Senegal SAS</strong> operates as a technology
+          services provider. It is neither licensed by the Central Bank of the
+          West African States (BCEAO) nor by the “Autorité des Marchés
+          Financiers de l’UMOA (AMF-UMOA)”, also known as the “Conseil Regional
+          de l’Epargne Publique et des Marches Financiers (CREPMF)”. However, it
+          is self-considered as an “Entreprise et Profession Non Financière
+          Désignée (EPNFD)” and is a reporting entity to the Financial
+          Intelligence Unit (CENTIF), under the control of the Ministry of
+          Economy and Finances, for anti-money laundering purposes.
         </p>
 
         <h3>
@@ -46,15 +64,19 @@
           Bulgaria and European Economic Area (EEA)
         </h3>
         <p>
-          <strong>MF European Trade EOOD (trading as MoneyFlap Europe)</strong>, is registered as a
-          <a target="_blank" href="https://nra.bg/wps/portal/nra/registers-i-spisuci/registers/page.virtualni.valuti/registur-na-licata-predstaviashti-uslugi-za-obmqna-megdu-virtualni-i-priznati-valuti">
+          <strong>MF European Trade EOOD (trading as MoneyFlap Europe)</strong>,
+          is registered as a
+          <a
+            target="_blank"
+            href="https://nra.bg/wps/portal/nra/registers-i-spisuci/registers/page.virtualni.valuti/registur-na-licata-predstaviashti-uslugi-za-obmqna-megdu-virtualni-i-priznati-valuti"
+          >
             Virtual Assets Service Provider (VASP)
           </a>
-          under No. 3170376608 in the Electronic Public Register of Persons of the National Revenue Agency (NRA) against money laundering. This registration is for the purposes of providing exchange services between virtual currencies and non-gold-backed currencies, as well as offering custodial wallet services. 
-        </p>
-        <p>
-          <strong>Disclaimer:</strong> blockchain technology and virtual
-          currency registrations are not yet required.
+          under No. 3170376608 in the Electronic Public Register of Persons of
+          the National Revenue Agency (NRA) against money laundering. This
+          registration is for the purposes of providing exchange services
+          between virtual currencies and non-gold-backed currencies, as well as
+          offering custodial wallet services.
         </p>
       </div>
     </div>
