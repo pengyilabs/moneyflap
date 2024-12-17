15411acb723e282b87a08e15123e7dd7d294b13d
dowar <gonzalo@dowar.xyz>
2024-12-17 00:33:54 -0600

    Add i18n support with Nuxt.js and configure localization files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,10 @@ export default defineNuxtConfig({
   typescript: {
     strict: false,
   },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    vueI18n: "./i18n.config.ts", // if you are using custom path, default
+  },
   // alias: {
   //   "@": path.resolve(__dirname, "src"),
   // },
